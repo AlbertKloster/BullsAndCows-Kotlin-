e32d8b9cbd0157d8c92b09f4592b9af360e23e95
AlbertKloster <albert.kloster@gmail.com>
2023-07-18 21:47:05 -0400

    Stage6

--- Main.kt
+++ Main.kt
@@ -4,14 +4,27 @@ import kotlin.random.Random
 
 fun main() {
     try {
-        println("Please, enter the secret code's length:")
-        val digits = readln().toInt()
+        println("Input the length of the secret code:")
+        val length = readln().toInt()
 
-        if (digits > 10)
-            throw RuntimeException("Error: can't generate a secret number with a length of $digits because there aren't enough unique digits.")
+        println("Input the number of possible symbols in the code:")
+        val symbols = readln().toInt()
 
-        val secretCode = getRandomNumber(digits)
+        if (length > symbols)
+            throw RuntimeException("Error: can't generate a secret number with a length of $length because there aren't enough unique digits.")
+
+        val secretCode = getRandomNumber(length, symbols)
         val grader = Grader(secretCode)
+
+        val possibleSymbols = if (symbols < 11) {
+            "(0-${symbols - 1})"
+        } else if (symbols == 11) {
+            "(0-9, a)"
+        } else {
+            "(0-9, a-${'a' + symbols - 11})"
+        }
+
+        println("The secret is prepared: ${"*".repeat(length)} $possibleSymbols.")
 
         println("Okay, let's start a game!")
         var turn = 1
@@ -25,7 +38,7 @@ fun main() {
             val gradeBulls = if (bulls > 0) "$bulls bull${if (bulls == 1) "" else "s"}${if (cows > 0) " and " else ""}" else ""
             val gradeCows = if (cows > 0) "$cows cow${if (cows == 1) "" else "s"}" else ""
             println("Grade: ${if (bulls == 0 && cows == 0) "None" else gradeBulls + gradeCows}")
-            if (bulls == digits) break
+            if (bulls == length) break
             turn++
         }
         println("Congratulations! You guessed the secret code.")
@@ -35,12 +48,13 @@ fun main() {
     }
 }
 
-private fun getRandomNumber(digits: Int): String {
-    val builder = StringBuilder(Random.nextInt(1, 10))
-    while (builder.length < digits) {
-        val nextDigit = Random.nextInt(10).toString().first()
-        if (builder.contains(nextDigit)) continue
-        builder.append(nextDigit)
+private fun getRandomNumber(length: Int, symbols: Int): String {
+    val builder = StringBuilder()
+    while (builder.length < length) {
+        val nextDigit = Random.nextInt(symbols)
+        val symbol = if (nextDigit < 10) nextDigit.toString().first() else 'a' + nextDigit - 10
+        if (builder.contains(symbol)) continue
+        builder.append(symbol)
     }
     return builder.toString()
 }
