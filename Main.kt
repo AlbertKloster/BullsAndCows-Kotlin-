f28211f1afafa8cff47e40b7d3e3920a821ee24c
AlbertKloster <albert.kloster@gmail.com>
2023-07-18 22:14:06 -0400

    Stage7

--- Main.kt
+++ Main.kt
@@ -5,26 +5,36 @@ import kotlin.random.Random
 fun main() {
     try {
         println("Input the length of the secret code:")
-        val length = readln().toInt()
+        val length = readln()
+        if (!length.matches(Regex("\\d+")))
+            throw RuntimeException("Error: \"$length\" isn't a valid number.")
+
+        if (length.toInt() < 1)
+            throw RuntimeException("Error: \"$length\" isn't a valid number.")
 
         println("Input the number of possible symbols in the code:")
-        val symbols = readln().toInt()
+        val symbols = readln()
+        if (!symbols.matches(Regex("\\d+")))
+            throw RuntimeException("Error: \"$symbols\" isn't a valid number.")
 
-        if (length > symbols)
-            throw RuntimeException("Error: can't generate a secret number with a length of $length because there aren't enough unique digits.")
+        if (symbols.toInt() > 36)
+            throw RuntimeException("Error: maximum number of possible symbols in the code is 36 (0-9, a-z).")
 
-        val secretCode = getRandomNumber(length, symbols)
+        if (length.toInt() > symbols.toInt())
+            throw RuntimeException("Error: it's not possible to generate a code with a length of $length with $symbols unique symbols.")
+
+        val secretCode = getRandomNumber(length.toInt(), symbols.toInt())
         val grader = Grader(secretCode)
 
-        val possibleSymbols = if (symbols < 11) {
-            "(0-${symbols - 1})"
-        } else if (symbols == 11) {
+        val possibleSymbols = if (symbols.toInt() < 11) {
+            "(0-${symbols.toInt() - 1})"
+        } else if (symbols.toInt() == 11) {
             "(0-9, a)"
         } else {
-            "(0-9, a-${'a' + symbols - 11})"
+            "(0-9, a-${'a' + symbols.toInt() - 11})"
         }
 
-        println("The secret is prepared: ${"*".repeat(length)} $possibleSymbols.")
+        println("The secret is prepared: ${"*".repeat(length.toInt())} $possibleSymbols.")
 
         println("Okay, let's start a game!")
         var turn = 1
@@ -38,7 +48,7 @@ fun main() {
             val gradeBulls = if (bulls > 0) "$bulls bull${if (bulls == 1) "" else "s"}${if (cows > 0) " and " else ""}" else ""
             val gradeCows = if (cows > 0) "$cows cow${if (cows == 1) "" else "s"}" else ""
             println("Grade: ${if (bulls == 0 && cows == 0) "None" else gradeBulls + gradeCows}")
-            if (bulls == length) break
+            if (bulls == length.toInt()) break
             turn++
         }
         println("Congratulations! You guessed the secret code.")
