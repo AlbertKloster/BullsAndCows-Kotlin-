c83d12a1ca9bf30d6b2aae15cc6ae7e1ae31369f
AlbertKloster <albert.kloster@gmail.com>
2023-07-18 21:11:42 -0400

    Stage4

--- Main.kt
+++ Main.kt
@@ -1,23 +1,36 @@
 package bullscows
 
 fun main() {
-    val digits = readln().toInt()
+    try {
+        println("Please, enter the secret code's length:")
+        val digits = readln().toInt()
 
-    if (digits > 10)
-        println("Error: can't generate a secret number with a length of $digits because there aren't enough unique digits.")
-    else
-        println("The random secret number is ${getPseudoRandomNumber(digits)}.")
+        if (digits > 10)
+            throw RuntimeException("Error: can't generate a secret number with a length of $digits because there aren't enough unique digits.")
 
-/*    val secretCode = "9305"
-    val grader = Grader(secretCode)
+        val secretCode = getPseudoRandomNumber(digits)
+        val grader = Grader(secretCode)
 
-    val code = readln()
-    val bulls = grader.countBulls(code)
-    val cows = grader.countCows(code)
+        println("Okay, let's start a game!")
+        var turn = 1
 
-    val gradeBulls = if (bulls > 0) "$bulls bull(s)${if (cows > 0) " and " else ""}" else ""
-    val gradeCows = if (cows > 0) "$cows cow(s)" else ""
-    println("Grade: ${if (bulls == 0 && cows == 0) "None" else gradeBulls + gradeCows}. The secret code is $secretCode")*/
+        while (true) {
+            println("Turn $turn:")
+            val code = readln()
+            val bulls = grader.countBulls(code)
+            val cows = grader.countCows(code)
+
+            val gradeBulls = if (bulls > 0) "$bulls bull${if (bulls == 1) "" else "s"}${if (cows > 0) " and " else ""}" else ""
+            val gradeCows = if (cows > 0) "$cows cow${if (cows == 1) "" else "s"}" else ""
+            println("Grade: ${if (bulls == 0 && cows == 0) "None" else gradeBulls + gradeCows}")
+            if (bulls == digits) break
+            turn++
+        }
+        println("Congratulations! You guessed the secret code.")
+
+    } catch (e: RuntimeException) {
+        println(e.message)
+    }
 }
 
 private fun getPseudoRandomNumber(digits: Int): String {
